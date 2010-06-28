6437f6acb5a71d6328d8e15105cd8bdb113142e8
Absol <andremelench@hotmail.com>
2010-06-28 09:56:58 -0500

    Código probado que obitene la tabla y las ips a partir del nombre de archivo

--- mx/cinvestav/p2p/utils/ServersListHandler.java
+++ mx/cinvestav/p2p/utils/ServersListHandler.java
@@ -9,38 +9,71 @@ import java.util.Hashtable;
 public class ServersListHandler {
 
 	private BufferedReader archivo = null;
+	private Hashtable<String, Integer> tabla = null;
+	private int contador = 0;
 
 	public ServersListHandler(String nombreArchivo) {
 		BufferedReader superParFile;
+
+		tabla = new Hashtable<String, Integer>();
 		try {
 			superParFile = new BufferedReader(new FileReader(nombreArchivo));
 			archivo = superParFile;
 		} catch (FileNotFoundException e) {
 			System.out.println("Archivo no encontrado");
-			e.printStackTrace();
+			tabla.put("127.0.0.1", new Integer(0));
+			contador = 0;
+			// e.printStackTrace();
 		}
 	}
 
 	/**
-	 * Regresa la siguiente ip en el archivo (solo la ip, no el peso) 
+	 * Regresa la siguiente ip en el archivo (solo la ip, no el peso)
 	 * */
 	public String getNextIp() {
-		String ipSuperPar = "127.0.0.1";
+		String ipSuperPar = null;
+		String tmps[];
+
+		if (archivo == null)
+			return "127.0.0.1";
+
 		try {
 			ipSuperPar = archivo.readLine();
-			
-			ipSuperPar = ipSuperPar.split("\t")[0];//se tokeniza y se toma solo el primer token
 		} catch (IOException e) {
 			System.out.println("No se pudo leer usando localhost");
 			e.printStackTrace();
 		}
 
-		return ipSuperPar;
+		if (ipSuperPar != null) {
+			tmps = ipSuperPar.split("\t");
+			tabla.put(tmps[0], new Integer(Integer.parseInt(tmps[1])));
+			contador++;
+			return tmps[0];
+		} else {
+			return null;
+		}
 	}
 
 	public Hashtable<String, Integer> getConexiones() {
-		// TODO Auto-generated method stub
-		return null;
+		if (archivo != null) {
+			String tmps[];
+
+			try {
+				String par = archivo.readLine();
+				while (par != null) {
+					tmps = par.split("\t");
+					tabla.put(tmps[0], new Integer(Integer.parseInt(tmps[1])));
+					contador++;
+
+					par = archivo.readLine();
+				}
+			} catch (IOException e) {
+				System.out.println("No se pudo leer archivo");
+				e.printStackTrace();
+			}
+		}
+
+		return tabla;
 	}
 
 }
