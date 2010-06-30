eac628219968349fa829bec8b5fbf7d1f7679f24
Absol <andremelench@hotmail.com>
2010-06-30 01:21:19 -0500

    Clase del cliente de bellman que envía el vector por el socket

--- mx/cinvestav/p2p/bellamFord/ClienteBellman.java
+++ mx/cinvestav/p2p/bellamFord/ClienteBellman.java
@@ -1,9 +1,56 @@
 package mx.cinvestav.p2p.bellamFord;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Hashtable;
+import java.util.Set;
+
 public class ClienteBellman {
+	private Hashtable<String, Integer> tabla;
+	private GironVectores giron;
 
 	public ClienteBellman(GironVectores giron) {
-		// TODO Auto-generated constructor stub
+		tabla = giron.actual;
+		this.giron = giron;
 	}
 
+	public void mandarVectorAVecinos(int puerto) {
+		HiloMandante hilo;
+		Set<String> enume = tabla.keySet();
+		for (String siguiente : enume) {
+			hilo = new HiloMandante(puerto, siguiente, giron);
+			(new Thread(hilo)).start();
+		}
+	}
+
+	public class HiloMandante implements Runnable {
+		private int puerto;
+		private String ip;
+		private GironVectores giron;
+
+		public HiloMandante(int puerto, String siguiente, GironVectores giron) {
+			this.puerto = puerto;
+			ip = siguiente;
+			this.giron = giron;
+		}
+
+		public void run() {
+			try {
+				Socket s = new Socket(ip, puerto);
+				OutputStream output = s.getOutputStream();
+				ObjectOutputStream objOut = new ObjectOutputStream(output);
+				giron.writeObject(objOut);
+				output.close();
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
