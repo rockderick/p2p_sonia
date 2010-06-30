6889a64e0e21d4578ff5daac62a5b08696ec8153
rockderick@gmail.com <rockderick@myhost.localdomain>
2010-06-30 16:52:59 +0800

    clase actualizada

--- mx/cinvestav/p2p/bellamFord/RodrigoServer.java
+++ mx/cinvestav/p2p/bellamFord/RodrigoServer.java
@@ -1,15 +1,89 @@
 package mx.cinvestav.p2p.bellamFord;
 
-public class RodrigoServer implements Runnable {
+import java.net.*;
+import java.io.*;
+
+public class RodrigoServer {
+
+
+        private GironVectores giron;
+        private ClienteBellman clienteBell;
+        private ServerSocket serverSocket;
+        private final static int PORT=5432;
 
 	public RodrigoServer(GironVectores giron, ClienteBellman clienteBell) {
-		// TODO Auto-generated constructor stub
+		this.giron=giron;
+		this.clienteBell=clienteBell;
 	}
+        
+        public RodrigoServer(){
+        }
+	
+	public void serve(){
+	   try{
+	      serverSocket=new ServerSocket(PORT);
+	      	   
+	   }catch(IOException ioe){
+	        ioe.printStackTrace();
+	   }
+	   
+	   while(true){
+	       try{
+	          Thread t=new Thread(new Handler(serverSocket.accept()));
+	          t.start(); 
+	       }catch(IOException ioe){
+	          ioe.printStackTrace();
+	       }
+	    
+	   }
+	} 
+	
+	
+      class Handler implements Runnable{
+           private Socket socket;
+      
+        
+           Handler(Socket socket){
+              this.socket=socket;
+            }
+      
+            public void run(){
+               GironVectores vectors=null;
+               ObjectInputStream ois=null;
+               try{
+                   InputStream is = socket.getInputStream(); 
+                   ois = new ObjectInputStream(is);
+                   vectors= (GironVectores) ois.readObject(); 
+                }catch(UnknownHostException e){
+                     e.printStackTrace();
+                }catch(IOException ioe){
+                     ioe.printStackTrace();
+                }catch(ClassNotFoundException cnfe) {
+                    cnfe.printStackTrace();
+                }finally{
+                   try{ 
+                      ois.close();
+                   }catch(IOException ioe){
+                       ioe.printStackTrace();
+                   }    
+                }
+                if(giron.cambiovector(vectors)){
+                       clienteBell.mandarVectorAVecinos(PORT);
+                       
+                }
+           
+            }
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
+}
+	
+	
+	public static void main(String args[]){
+	    RodrigoServer rockServer=new RodrigoServer();          
+	    rockServer.serve(); 
 	}
 
 }
+
+
+
+
